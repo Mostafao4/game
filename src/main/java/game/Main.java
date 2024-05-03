dfcb28d22a960eeb028abd7ce8f4d7eb683f78a0
Ezzinhoooo <163548505+Ezzinhoooo@users.noreply.github.com>
2024-05-03 20:37:14 +0300

    class lion -getters&setters-methods::multiplyscore totalscore getreward

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -5,6 +5,7 @@ import game.engine.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dice Realms: Quest for the Elemental Crests!");
+        System.out.print(multiplyScore(3,4,));
 
         // CLIGameController cliGameController = new CLIGameController();
         // cliGameController.startGame();
