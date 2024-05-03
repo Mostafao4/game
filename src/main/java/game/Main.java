add900c9dc8003d57d75d62e0139455917c972ff
Ezzinhoooo <163548505+Ezzinhoooo@users.noreply.github.com>
2024-05-03 20:39:31 +0300

    Update Main.java

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -4,8 +4,7 @@ import game.engine.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dice Realms: Quest for the Elemental Crests!");
-        System.out.print(multiplyScore(3,4,));
+        System.out.println("Dice Realms: Quest for the Elemental Crests!");     
 
         // CLIGameController cliGameController = new CLIGameController();
         // cliGameController.startGame();
