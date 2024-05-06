8e9b1422a86812261eacb1ed3e4ef068a34e55ab
Mostafao4 <125620390+Mostafao4@users.noreply.github.com>
2024-05-06 22:38:55 +0200

    Update Phoenix.java

--- src/main/java/game/creatures/Phoenix.java
+++ src/main/java/game/creatures/Phoenix.java
@@ -1,4 +1,26 @@
 package game.creatures;
+import game.dice.*;
+import game.collectibles.*;
 
-public class Phoenix {
+public class Phoenix extends Creature {
+    int health;
+    int count=0;
+    MagentaDice dice;
+    int[] attack;
+    Reward[] rewards;
+    int[] score;
+
+    public Phoenix(int health,int[] array){
+
+    }
+
+    public int Attack() {
+        dice.getValue
+    }
+    public static collectibles Rewards(int index , Collectibles[] reward){
+       return  reward[index];
+    }
+    public static int getPoints(){
+
+    }
 }
