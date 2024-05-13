fe865c7330551e4804c3b70610e7b24ac08880c2
Ezzinhoooo <163548505+Ezzinhoooo@users.noreply.github.com>
2024-05-14 02:40:25 +0300

    lion class
    edited lion class-added config files

--- src/main/java/game/creatures/Lion.java
+++ src/main/java/game/creatures/Lion.java
@@ -2,62 +2,152 @@ package game.creatures;
 import java.util.*;
 import game.collectibles.*;
 import game.dice.Dice;
+import game.dice.DiceStatus;
 import game.dice.YellowDice;
 import game.engine.Move;
 import game.engine.Player;
-public class Lion {
+import java.io.IOException;
+
+public class Lion extends Creature{
    
 
 
     private static int hitNum=-1;
     private static int[] diceNum={0,0,0,0,0,0,0,0,0,0,0};
-    private int [] multipliers={1,1,1,2,1,1,2,1,2,1,3};
-    private int score;
+    private static int [] multipliers={0,0,0,0,0,0,0,0,0,0,0};
     private static int totalScore=0; 
-    private String[] rewards= {null , null, "TimeWarp",null, "RedBonus", "ArcaneBoost",null , "ElementalCrest", null,  "MagnetaBonus",  null}; 
-    private Move[] x =new Move[1];
-    
+    private static String[] rew=new String[11]; 
+    private static String[] mult=new String[11];
     public Lion( ) {
        
     }
+ //creates array rew type string from config for rewards
+public static void editReward(){
+        Properties prop = new Properties();
+    try{
+        prop.load(Lion.class.getClassLoader().getResourceAsStream("RadiantSvannaRewards.Properties"));
+        
+        for (int i=0;i<11;i++){
+            String r=prop.getProperty("hit"+i+1+"Reward");
+            rew[i]=r;
+        } 
+    }
+    catch (IOException ex) {
+        ex.printStackTrace();
+    }
+}
+//creates array mult type string from config for multipliers and edits multiplier array type int
+public static void editMultipliers(){
+        Properties prop = new Properties();
+    try{
+        prop.load(Lion.class.getClassLoader().getResourceAsStream("RadiantSvannaMultipliers.Properties"));
+        
+        for (int i=0;i<11;i++){
+            String m=prop.getProperty("hit"+i+1+"multiplier");
+            mult[i]=m;
+        } 
+    }
+    catch (IOException ex) {
+        ex.printStackTrace();
+    }
+    for (int i=0;i<11;i++){
+        if (mult[i]=="null"){
+            multipliers[i]=1;
+            mult[i]="";
+        }
+        else{
+            multipliers[i]=Integer.parseInt(mult[i]);
+            mult[i]="x"+mult[i];
+        }
+    }
+}
 
-public static void move(Dice dice){
+
+//increments hitnum(counter) & adds dice value to array diceNum 
+public static void move(YellowDice dice){
     hitNum++;
     diceNum[hitNum]=(int)dice.getValue();
 }
-public int getYellowRealmScore(){
-int score= multiplyScore(hitNum,diceNum, multipliers);
-return getScore( score, totalScore);
-}
 
+
+//uses multiplyScore & getScore methods to return total score of lion class
+public int getYellowRealmScore(){
+    int score= multiplyScore(hitNum,diceNum, multipliers);
+    return getScore( score, totalScore);
+}
+// gets the new score of the round after multiplying it
 public static int multiplyScore(int hitNum,int diceNum[],int[] multipliers){
     int score=multipliers[hitNum]*diceNum[hitNum];
      return score;
  }
+ //calculates total score
  public int getScore(int score, int totalScore){
      totalScore+=score;
      return totalScore;
  }
-public String getReward(int hitNum, String[] rewards){
-    return rewards[hitNum];
+ // checks rewards and returns the supposed reward & makes used reward an x
+public Reward getReward(){
+    switch (rew[hitNum]){
+    case "ArcaneBoost":
+            rew[hitNum]="X";
+            return ArcaneBoost;
+    case "BlueBonus": 
+            rew[hitNum]="X";
+            return BlueBonus;
+    case "ElementalCrest": 
+            rew[hitNum]="X";
+            return ElementalCrest;
+    case "EssenceBonus":
+            rew[hitNum]="X";
+            return EssenceBonus;
+    case "GreenBonus":
+            rew[hitNum]="X";
+            return GreenBonus;
+    case "MagentaBonus": 
+            rew[hitNum]="X";
+            return MagentaBonus;
+    case "RedBonus": 
+            rew[hitNum]="X";
+            return RedBonus;
+    case "TimeWarp":
+            rew[hitNum]="X";
+            return TimeWarp;
+    case "YellowBonus": 
+            rew[hitNum]="X";
+            return YellowBonus;
+    default: return null;
+    }
  }
+ // returns an array type move with all the possible moves before rolling the dice
  public Move[] getAllPossibleMoves(){
-    if (hitNum<11)
-    this.x[0]=6;
-    else
-     x[0]=1;
-     return x;
-
+    if (hitNum<11){
+    Move[] pm1=new Move[6];
+    for(int i=0;i<pm1.length;i++){
+        pm1[i]=new Move(new YellowDice(i+1),Realm.YELLOW);
+    }
+    return pm1;
+}
+    else{
+    Move[] pm2=new Move[1];
+    pm2[0]=new Move(0,DiceStatus.YELLOW);
+    return pm2;
+    }
  }
+ //returns possible moves when dice is rolled (dicenumber)
+ public Move[] getPossibleMovesForADie(YellowDice dice){
+    return new Move(dice,Realm.YELLOW);
+ }
+//returns the scoresheet & creates string for rewards & multipliers
  public String getScoreSheet(){
-
+    editReward();
+    editMultipliers();
    return "Radiant Savanna: Solar Lion (YELLOW REALM):\n" +
     "+-----------------------------------------------------------------------+\n" +
     "|  #  |1    |2    |3    |4    |5    |6    |7    |8    |9    |10   |11   |\n" +
     "+-----------------------------------------------------------------------+\n" +
     "|  H  |"+diceNum[0]*multipliers[0]+"    |"+diceNum[1]*multipliers[1]+"    |"+diceNum[2]*multipliers[2]+"    |"+diceNum[3]*multipliers[3]+"    |"+diceNum[4]*multipliers[4]+"    |"+diceNum[5]*multipliers[5]+"    |"+diceNum[6]*multipliers[6]+"    |"+diceNum[7]*multipliers[7]+"    |"+diceNum[8]*multipliers[8]+"    |"+diceNum[9]*multipliers[9]+"    |"+diceNum[10]*multipliers[10]+"    |\n" +
-    "|  M  |     |     |     |x2   |     |     |x2   |     |x2   |     |x3   |\n" +
-    "|  R  |     |     |TW   |     |RB   |AB   |     |EC   |     |MB   |     |\n" +
+    "|  M  |"+mult[0]+     "|"+mult[1]+     "|"+mult[2]+     "|"+mult[3]+    "|"+mult[4]+     "|"+mult[5]+     "|"+mult[6]+    "|"+mult[7]+     "|"+mult[8]+     "|"+mult[9]+     "|"+mult[10]+    "|\n" +
+    "|  R  |"+rew[0]+"    |"+rew[1]+"     |"+rew[2]+"     |"+rew[3]+"    |"+rew[4]+"   |"+rew[5]+"    |"+rew[6]+"    |"+rew[7]+"    |"+rew[8]+"    |"+rew[9]+"    |"+rew[10]+"   |\n" +
     "+-----------------------------------------------------------------------+\n\n";
 
  }
