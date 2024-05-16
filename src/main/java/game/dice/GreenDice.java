3af3170e54fc16d5dd38f4c3f9a6641091d26e33
Majed <majed.salem@student.guc.edu.eg>
2024-05-16 23:39:06 +0200

    Updates

--- src/main/java/game/dice/GreenDice.java
+++ src/main/java/game/dice/GreenDice.java
@@ -1,10 +1,24 @@
 package game.dice;
 
+import game.engine.GameBoard;
+
 public class GreenDice extends Dice{
     int realValue;
 
     public GreenDice(int value){
         super(value);
+        //realValue = value + ArcanePrism.getValue();
+    }
+
+    public int getRealValue(){
+        return this.realValue;
+    }
+
+    private boolean checkAvailability(ArcanePrism die){
+        if(die.getDiceStatus().equals(DiceStatus.AVAILABLE)){
+            return true;
+        }
+        return false;
     }
 
 }
