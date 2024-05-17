3a21125feab7e1dc91cce400e2ec8898d036dc29
Majed <majed.salem@student.guc.edu.eg>
2024-05-17 22:34:19 +0200

    Update Gaia.java

--- src/main/java/game/creatures/Gaia.java
+++ src/main/java/game/creatures/Gaia.java
@@ -33,6 +33,8 @@ public class Gaia extends Creature{
     private boolean[] verticalBonus = new boolean[4];
     private boolean[] horizontalBonus = new boolean[3];
     private int[] scores;
+    private String[] rowStrings;
+    private String[] colStrings;
 
 
     public Gaia(){
@@ -41,13 +43,13 @@ public class Gaia extends Creature{
         rowRewards = new Reward[3];
         columnRewards = new Reward[4];
         scores = new int[11];
+        rowStrings = new String[3];
+        colStrings = new String[4];
         readConfigRewards();
         readConfigScores();
     }
 
     private void readConfigRewards(){
-        String[] rowStrings = new String[3];
-        String[] colStrings = new String[4];
 
         Properties prop1 = new Properties();
 
@@ -301,19 +303,100 @@ public class Gaia extends Creature{
         + "|  #  |1    |2    |3    |4    |R    | \n"
         + "+-----------------------------------+ \n"
         // + "|  1  |X    |2    |3    |4    |YB   | \n"
-        + "|  1  " + scoreSheetHelper(0) + "|" + (horizontalBonus[0]?"X":"YB") + "   |\n"
+        + "|  1  " + scoreSheetHelper(0) + "|" + (horizontalBonus[0]?"X":initialsRows(0)) + "   |\n"
         // + "|  2  |5    |6    |7    |8    |RB   | \n"
-        + "|  2  " + scoreSheetHelper(1) + "|" + (horizontalBonus[1]?"X":"RB") + "   |\n"
+        + "|  2  " + scoreSheetHelper(1) + "|" + (horizontalBonus[1]?"X":initialsRows(1)) + "   |\n"
         // + "|  3  |9    |10   |11   |12   |EC   | \n"
-        + "|  3  " + scoreSheetHelper(2) + "|" + (horizontalBonus[2]?"X":"EC") + "   |\n"
+        + "|  3  " + scoreSheetHelper(2) + "|" + (horizontalBonus[2]?"X":initialsRows(2)) + "   |\n"
         + "+-----------------------------------+ \n"
-        + "|  R  |TW   |BB   |MB   |AB   |     | \n"
+        //+ "|  R  |TW   |BB   |MB   |AB   |     | \n"
+        + "|  R  " + scoreSheetHelper3() + "| \n"
         + "+-----------------------------------------------------------------------------+ \n"
-        + "|  S  |0    |1    |2    |4    |7    |11   |16   |22   |29   |37   |46   |56   | \n"
+        //+ "|  S  |0    |1    |2    |4    |7    |11   |16   |22   |29   |37   |46   |56   | \n"
+        + "|  S  |0    " + scoreSheetHelper2() + "|\n"
         + "+-----------------------------------------------------------------------------+ \n\n";
         return s;
     }
     
+    private String scoreSheetHelper2(){
+        String s = "";
+        for(int i = 0; i < scores.length; i++){
+            s+= "|" + scores[i];
+            s+= (scores[i] < 10)?"    ":"   ";
+        }
+        return s;
+    }
+
+    private String scoreSheetHelper3(){
+        String s = "";
+        for(int i = 0; i < verticalBonus.length; i++){
+            if(!verticalBonus[i]){
+                switch(i){
+                    case 0:
+                        s+="|" + initialsColumns(0);
+                        break;
+                    case 1:
+                        s+="|" + initialsColumns(1);
+                        break;
+                    case 2:
+                        s+="|" + initialsColumns(2);
+                        break;
+                    case 3:
+                        s+="|" + initialsColumns(3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+                s+= "X   ";
+        }
+        s+="|     ";
+        return s;
+    }
+
+    private String initialsRows(int num){
+        switch(rowStrings[num]){
+            case "RedBonus":
+                return "RB";
+            case "YellowBonus":
+                return "YB";
+            case "BlueBonus":
+                return "BB";
+            case "MagentaBonus":
+                return "MB";
+            case "ElementalCrest":
+                return "EC";
+            case "ArcaneBoost":
+                return "AB";
+            case "TimeWarp":
+                return "TW";
+            default:
+                return null;
+        }
+    }
+
+    private String initialsColumns(int num){
+        switch(colStrings[num]){
+            case "RedBonus":
+                return "RB   ";
+            case "YellowBonus":
+                return "YB   ";
+            case "BlueBonus":
+                return "BB   ";
+            case "MagentaBonus":
+                return "MB   ";
+            case "ElementalCrest":
+                return "EC   ";
+            case "ArcaneBoost":
+                return "AB   ";
+            case "TimeWarp":
+                return "TW   ";
+            default:
+                return null;
+        }
+    }
+
     private boolean[][] checkZero(){
         boolean[][] zeros = new boolean[3][4];
         for(int i = 0; i < gaias.length; i++){
@@ -325,23 +408,14 @@ public class Gaia extends Creature{
         return zeros;
     }
     private String scoreSheetHelper(int rowNum){
-        boolean[][] scores = checkZero();
+        boolean[][] scoresBool = checkZero();
         String s = "";
-        if(rowNum != 2){
-            for(int i = 0; i < scores[rowNum].length; i++){
-                if(scores[rowNum][i])
-                    s+= "|X    ";
-                else
-                    s+= "|" + this.gaias[rowNum][i] + "    ";
-            }
-        } else {
-            for(int i = 0; i < scores[rowNum].length; i++){
-                if(scores[rowNum][i])
+            for(int i = 0; i < scoresBool[rowNum].length; i++){
+                if(scoresBool[rowNum][i])
                     s+= "|X    ";
                 else 
                     s+= "|" + this.gaias[rowNum][i] + ((this.gaias[rowNum][i] > 9)?"   ":"    ");;
             }
-        }
         
         return s;
     }
