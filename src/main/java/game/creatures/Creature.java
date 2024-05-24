7e912913c8812d3ca89eaf6909d9adfdf81f1307
Yassin-Fakhry <yassinfakhry2005@gmail.com>
2024-05-24 20:36:41 +0200

    Exceptions

--- src/main/java/game/creatures/Creature.java
+++ src/main/java/game/creatures/Creature.java
@@ -3,10 +3,12 @@ package game.creatures;
 import game.collectibles.Reward;
 import game.dice.Dice;
 import game.engine.Move;
+import game.exceptions.InvalidDiceSelectionException;
+import game.exceptions.InvalidMoveException;
 
 public abstract class Creature {
-    public abstract boolean makeMove(Move move) ;
-    public abstract Move[] getAllPossibleMoves();
-    public abstract Move[] getPossibleMovesForADie(Dice dice) ;
+    public abstract boolean makeMove(Move move) throws InvalidMoveException;
+    public abstract Move[] getAllPossibleMoves() throws InvalidMoveException;
+    public abstract Move[] getPossibleMovesForADie(Dice dice) throws InvalidDiceSelectionException;
     public abstract Reward[] checkReward();
 }
