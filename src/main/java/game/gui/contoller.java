193529112d7a55a5ff1da3351721ef813ca94df4
Youssef Elshamy <youssefsabry754@gmail.com>
2024-05-27 01:10:11 +0200

    Gui edits

---
+++ src/main/java/game/gui/contoller.java
@@ -0,0 +1,14 @@
+package game.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class contoller {
+    @FXML
+    
+    public void up(ActionEvent event){
+        System.out.println("up");
+    }
+
+}
